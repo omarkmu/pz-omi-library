8edc635495197a889dfc2282918d9559e2ccf9a0
Omar <omarkmu@gmail.com>
2026-02-02 14:37:13 -0500

    Add `schema.fromJsonFile`

--- Contents/mods/OmiLibrary/42.13/media/lua/shared/OmiLibrary/Component/Schema/Schema.lua
+++ Contents/mods/OmiLibrary/42.13/media/lua/shared/OmiLibrary/Component/Schema/Schema.lua
@@ -111,7 +111,7 @@ end
 ---@return table? result
 ---@return string? error
 function Schema:readFile(fileReadOptions, schemaReadOptions)
-    local decoded, err = json.readObject(fileReadOptions)
+    local decoded, err = json.tryReadObject(fileReadOptions)
     if not decoded then
         return nil, err
     end
@@ -177,12 +177,14 @@ return Schema
 
 --#region Type Definitions
 
----@class Args.Schema
----@field properties table<string, schema.Field> The schema properties.
+---@class Args.Schema.Partial
 ---@field transforms? Schema.Callback.Transform[] List of transforms to apply to source data.
----@field form? Args.FormGenerator.Partial The form generator definition.
 ---@field onRead? Schema.Callback Invoked after reading a table against the schema.
 ---@field sanitize? Schema.Callback Invoked to sanitize values to prepare for writing.
+
+---@class Args.Schema : Args.Schema.Partial
+---@field properties table<string, schema.Field> The schema properties.
+---@field form? Args.FormGenerator.Partial The form generator definition.
 
 
 ---@class Schema.Callbacks
--- Contents/mods/OmiLibrary/42.13/media/lua/shared/OmiLibrary/Module/Schema.lua
+++ Contents/mods/OmiLibrary/42.13/media/lua/shared/OmiLibrary/Module/Schema.lua
@@ -3,6 +3,8 @@
 
 local core = require 'OmiLibrary/Module/Utils'
 local json = require 'OmiLibrary/Module/JSON'
+local libLog = require 'OmiLibrary/Component/Logging/LibraryLogger'
+local isempty = table.isempty
 
 ---@class(partial) Schema
 local Schema = require 'OmiLibrary/Component/Schema/Schema'
@@ -65,7 +67,7 @@ function schema.bool(defaultOrOptions)
         defaultOrOptions = { default = not not defaultOrOptions }
     end
 
-    return schema.BooleanField:new(defaultOrOptions)
+    return schema.BooleanField:new(defaultOrOptions --[[@as Args.BooleanField)]])
 end
 
 ---Helper which creates a color configuration field.
@@ -109,6 +111,67 @@ function schema.double(defaultOrOptions, min, max)
     defaultOrOptions.min = min or defaultOrOptions.min
     defaultOrOptions.max = max or defaultOrOptions.max
     return schema.DoubleField:new(defaultOrOptions)
+end
+
+---Helper to create a schema from a JSON file.
+---@param options Args.Schema.FromFile
+---@return Schema
+function schema.fromJsonFile(options)
+    local raw, err = json.tryReadObject({
+        modId = options.modId,
+        filename = options.filename or 'configuration.json',
+        reader = options.reader,
+    })
+
+    local log = options.log or libLog
+    local suffix = log == libLog and (', mod: ' .. options.modId) or ''
+    if not raw then
+        log.fatal('Failed to create schema: %s%s', err or 'an error occurred', suffix)
+        ---@cast raw -?
+    end
+
+    if options.preprocess then
+        raw = options.preprocess(raw)
+    end
+
+    local version = raw.SCHEMA_VERSION ---@type integer
+    if type(version) ~= 'number' then
+        version = 1
+    end
+
+    ---@type table<string, schema.Field>
+    local properties = { VERSION = schema.int(version) }
+
+    local fields = raw.properties or {}
+    local rules = {} ---@type table<string, forms.Rules>
+    for i = 1, #fields do
+        local prop = fields[i]
+        if type(prop) ~= 'table' then
+            prop = { type = 'missing' }
+        end
+
+        if prop.type ~= 'object' then
+            -- only objects allowed for now, until forms support an automatic page for top-level fields
+            log.error('Invalid top-level property type for %s (%s)%s', prop.name, prop.type, suffix)
+        else
+            properties[prop.name], rules[prop.name] = schema._generateField(prop, options, raw)
+        end
+    end
+
+    local form = core.copy(options.form) --[[@as Args.FormGenerator.Partial]]
+    form.rules = rules
+
+    if not form.prefix then
+        form.prefix = options.modId .. '.config'
+    end
+
+    return schema.new({
+        properties = properties,
+        form = form,
+        onRead = options.onRead,
+        sanitize = options.sanitize,
+        transforms = options.transforms,
+    })
 end
 
 ---Helper which creates an integer configuration field.
@@ -159,7 +222,7 @@ function schema.string(defaultOrOptions)
         defaultOrOptions = { default = defaultOrOptions }
     end
 
-    return schema.StringField:new(defaultOrOptions)
+    return schema.StringField:new(defaultOrOptions --[[@as Args.StringField)]])
 end
 
 ---Helper which creates a string enumeration configuration field.
@@ -168,6 +231,141 @@ end
 function schema.stringEnum(options)
     return schema.StringEnumField:new(options)
 end
+
+
+---Generates a schema field from a definition table defined in a JSON file.
+---@param def JSONSchemaProperty
+---@param options Args.Schema.FromFile
+---@param raw table
+---@param parentKey string?
+---@return schema.Field?, forms.Rules?
+---@private
+function schema._generateField(def, options, raw, parentKey)
+    local log = options.log or libLog
+    local suffix = log == libLog and (', mod: ' .. options.modId) or ''
+    local properties = {} ---@type table<string, schema.Field>
+    local childRules = {} ---@type table<string, forms.Rules>
+
+    local rules = core.copy(def) --[[@as forms.Rules]]
+    local rulesAsAny = rules --[[@as any]]
+    rulesAsAny.name = nil
+    rulesAsAny.type = nil
+    rulesAsAny.default = nil
+    rulesAsAny.defaultAll = nil
+    rulesAsAny.maxItems = nil
+    rulesAsAny.options = nil
+    rulesAsAny.properties = nil
+
+    local props = def.properties
+    local key = (parentKey and (parentKey .. '.') or '') .. def.name
+    if props then
+        for i = 1, #props do
+            local child = props[i]
+            properties[child.name], childRules[child.name] = schema._generateField(child, options, raw, key)
+        end
+    end
+
+    if not isempty(childRules) then
+        rules.children = childRules
+    end
+
+    local field = options.convert and options.convert(def, rules, key, raw)
+
+    local _type = field and '' or def.type
+    if _type == 'object' then
+        field = schema.container(properties)
+    elseif _type == 'object-list' then
+        field = schema.array({
+            maxItems = def.maxItems,
+            items = schema.object({
+                skipMissing = true,
+                properties = properties,
+            }),
+        })
+    elseif _type == 'string' or _type == 'textbox' then
+        field = schema.string(def.default)
+
+        if _type == 'textbox' then
+            rules.displayLines = 10
+        end
+    elseif _type == 'compatibility' then
+        field = schema.compatibility(def.default)
+    elseif _type == 'color' then
+        local default
+        local color = def.default
+        if color then
+            default = { r = color[1], g = color[2], b = color[3] }
+        end
+
+        field = schema.color({ default = default })
+    elseif _type == 'checkbox' then
+        field = schema.bool(def.default)
+    elseif _type == 'page-checkbox' then
+        field = schema.bool(def.default)
+        rules.togglePageFields = true
+    elseif _type == 'checkbox-group' then
+        local default
+        if def.default then
+            default = def.default
+        elseif def.defaultAll then
+            default = def.options
+        end
+
+        field = schema.set({
+            default = core.set.table(default),
+            items = schema.stringEnum({ values = def.options }),
+        })
+    elseif _type == 'string-dropdown' then
+        field = schema.stringEnum({
+            default = def.default,
+            values = def.options,
+        })
+    elseif _type == 'string-map' then
+        field = schema.object({
+            skipMissing = true,
+            default = def.default,
+            additionalProperties = schema.string(),
+        })
+    elseif _type == 'string-list' then
+        field = schema.array({
+            items = schema.string(),
+            default = def.default,
+            maxItems = def.maxItems,
+        })
+    elseif _type == 'integer' or _type == 'number' then
+        local createField = _type == 'integer' and schema.int or schema.double
+
+        local max = def.max
+        local min = def.min
+        local default = def.default
+
+        if not max then
+            max = 2147483647
+            log.error('Missing max value for property %s%s', key, suffix)
+        end
+
+        if not min then
+            min = 0
+            log.error('Missing min value for property %s%s', key, suffix)
+        end
+
+        if not default then
+            default = 0
+            log.error('Missing default value for property %s%s', key, suffix)
+        end
+
+        field = createField(default, min, max)
+    elseif not field then
+        log.error('Unknown type %s for property %s%s', _type, key, suffix)
+    end
+
+    if isempty(rules) then
+        return field
+    end
+
+    return field, rules
+end
+
 
 setmetatable(schema, { __call = function(self, ...) return self.new(...) end })
 return schema
@@ -185,5 +383,28 @@ return schema
 ---@field schema Schema The parent schema of the field.
 ---@field value? any The value of the field in the source table.
 ---@field skipMissing boolean If `true`, missing fields will not be included as defaults.
+
+---@class Args.Schema.FromFile : Args.Schema.Partial
+---@field modId string The ID of the mod to read from.
+---@field filename? string The filename of the file to read. Defaults to `configuration.json`.
+---@field reader? BufferedReader The reader to use. If present, `filename` will be ignored.
+---@field log? Logger A logger to use for errors. Failures to open or decode the file will be treated as fatal.
+---@field form? Args.FormGenerator.FromFile The form generator definition.
+---@field onRead? Schema.Callback Invoked after reading a table against the schema.
+---@field sanitize? Schema.Callback Invoked to sanitize values to prepare for writing.
+---@field preprocess? fun(raw: table): table Preprocessor for the table loaded from the file.
+---@field convert? fun(prop: JSONSchemaProperty, rules: forms.Rules, key: string, raw: table): schema.Field Conversion function for properties defined in JSON.
+---`raw` is the entire table from JSON.
+
+---@class JSONSchemaProperty : forms.Rules.FromFile
+---@field name string The name of the property.
+---@field type string The type of the property.
+---@field default? any The default value of the field.
+---@field max? any The maximum value of the field.
+---@field min? any The minimum value of the field.
+---@field options? any Options for a field that accepts options.
+---@field maxItems? integer The maximum number of items for a field with items.
+---@field defaultAll? boolean Whether to use all options as the default value, for a checkbox group.
+---@field properties? JSONSchemaProperty[] Child properties.
 
 --#endregion
